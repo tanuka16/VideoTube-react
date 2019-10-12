94b5c0e7c78e7b04ddb12a1860311d35dd155df1
Tanuka16 <das.tanuka@yahoo.com>
2019-10-11 22:48:26 -0400

    add api key

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React, {Component} from 'react';
 import './App.css';
 import SearchBar from './components/search_bar';
+import YTSearch from 'youtube-api-search';
+
+const API_KEY = 'AIzaSyAlbkgkqiiQMsLH3ZjlzqkynHH06DX3Sjw';
 
 class App extends Component {
+ constructor(props){
+    super(props)
+//method for youtube search api; 1st argument will be a key,which has 2 other arguement (1)key, (2)arguement what we're searching for(song);
+// arrow function gets the data from the server
+    YTSearch({key:API_KEY, term: 'songs'}, (data)=>{
+      console.log(data);
+    })
+  }
+
   render(){
     return(
       <div className="App">
