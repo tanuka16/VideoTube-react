72a454e81ef80dd7aa1d0b42db1f4b87346209b6
Tanuka16 <das.tanuka@yahoo.com>
2019-10-12 21:10:13 -0400

    added youtube videos

---
+++ src/components/video_list.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import VideoListItem from './video_list_items';
+
+
+const VideoList = (props)=>{
+//iterate through the list of videos
+  const videoItem = props.videos.map((video)=>{
+    return <VideoListItem key={video.etag} video={video}/>
+  })
+
+  return(
+    //return a video list
+    <ul className="col-md-4 list-group">
+        {videoItem}
+        {/* {props.videos.length}*/}
+    </ul>
+  )
+}
+export default VideoList;
---
+++ src/components/video_list_items.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+//replace argument 'props' with destructure method('video'), which allow us to call only 1 object that we want
+//in this case we'll get the video atribute from video_list, not the entire list of videos
+const VideoListItem = ({video}) =>{
+  const imageUrl=video.snippet.thumbnails.default.url;
+  return (
+    <li className="list-group-item" >
+      <div className="video-list media">
+        <div className="media-left">
+          <img className="media-left" src={imageUrl}/>
+        </div>
+        <div className="media-body">
+            <div className="media-heading">
+              {video.snippet.title}
+            </div>
+
+        </div>
+      </div>
+    </li>
+  )
+}
+
+export default VideoListItem;
